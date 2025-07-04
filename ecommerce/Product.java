9ab03bd9c6f3ebbe1d603299593429dbbb655219
Mariam Abdelgalil <88682947+mariamabdelgalil@users.noreply.github.com>
2025-07-04 17:39:35 +0300

    Add product class.

--- ecommerce/Product.java
+++ ecommerce/Product.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ecommerce;
 
-/**
- *
- * @author maria
- */
 public abstract class Product {
     protected String name;
     protected double price;
