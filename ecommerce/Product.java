fff684b2129a7ab7fb2d79c3d8f099a71861986a
Mariam Abdelgalil <88682947+mariamabdelgalil@users.noreply.github.com>
2025-07-04 17:35:01 +0300

    Add product class.

---
+++ ecommerce/Product.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ecommerce;
+
+/**
+ *
+ * @author maria
+ */
+public abstract class Product {
+    protected String name;
+    protected double price;
+    protected int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public int getQuantity() { return quantity; }
+
+    public void reduceQuantity(int amount) {
+        if (amount <= quantity) quantity -= amount;
+    }
+
+    public abstract boolean isExpired();
+    public abstract boolean requiresShipping();
+}
+
